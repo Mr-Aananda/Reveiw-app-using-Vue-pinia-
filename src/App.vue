75675aad45eaa6d89255d047f99d4c4521f73275
Mr Aananda <rmananda007@gmail.com>
2023-09-12 18:14:02 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+
+<template>
+  <div>
+    <Header/>
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+import { RouterView } from 'vue-router';
+import Header from './components/Header.vue';
+import { useReviewsStore } from './stores/reviews';
+
+const store = useReviewsStore();
+
+onMounted(() => {
+  store.fetchReviews();
+});
+</script>
+
---
+++ src/components/ReviewList.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <p v-if="!store.reviewsContent || store.reviewsContent.lenght===0">No Reviews Yet.</p>
+        <div v-else class="reviewList">
+            <review-item
+            v-for="(item, i) in store.reviewsContent"
+            :key="i"
+            :item="item"
+            />
+
+        </div>
+
+    </div>
+</template>
+
+<script setup>
+import ReviewItem from './ReviewItem.vue';
+import { useReviewsStore } from '../stores/reviews';
+
+const store = useReviewsStore();
+
+</script>
+
+<style scoped>
+
+</style>
